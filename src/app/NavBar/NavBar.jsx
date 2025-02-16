69547dc0b70e9a979073f29fb4871974dc12d733
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-02-16 18:25:17 +0600

    Created navbar & footer

---
+++ src/app/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Me</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
